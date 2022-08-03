5c716fae8c4a09ba4977b39129c0897f0b0a41a5
xfrcc <47122480+xfrcc@users.noreply.github.com>
2022-08-03 15:53:01 -0400

    Refactor + better html parsign

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 const axios = require('axios');
+const cheerio = require('cheerio');
 const url = require('url');
+const fs = require('fs');
 
-const ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36";
+const stream = fs.createWriteStream('logs.txt', { flags:'a' });
+const ua = 'Mozilla/5.0 (Linux; Android 10; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Mobile Safari/537.36';
+
+const simpleLog = (message) => {
+    const fecha = new Date().toLocaleString('en-US').replace(/,/, '');
+    stream.write(`[${fecha}]: ${message}\n`);
+}
+
+
 const main = async() => {
     try {
-        const res = await axios.get('https://sv.spliktv.xyz/activar', {
+        let res = await axios({
+            method: 'get',
+            url: 'https://sv.spliktv.xyz/activar',
             headers: {
-                "user-agent": ua
+                'User-Agent': ua,
+                "Referer": "https://sv.spliktv.xyz/activar",
+                "Origin": "https://sv.spliktv.xyz"
             }
         });
-        const body = res.data;
-        if (body.includes('Ya activaste!')) {
-            console.log('Ya activaste, reintentando en 1 hora...');
-            setTimeout(main,  1000 * 60 * 60);
-        }
-        else {
-            const id = body.substring(body.indexOf(".setAttribute('name', '")+".setAttribute('name', '".length, body.indexOf("');"));
-            console.log(`Id es: ${id}, activando...`);
-            params = {
+        let html = res.data;
+        let $ = cheerio.load(html);
+        const msgActivaste = $('#success').first().text().length > 0 ? $('#success').first().text() : null;
+        const msgHora = $('.info').first().text().length > 0 ? $('.info').first().text() : null;
+        if (!(msgActivaste || msgHora)) {
+            const id = html.substring(html.indexOf(".setAttribute('name', '")+".setAttribute('name', '".length, html.indexOf("');"));
+            console.log(`ID es: ${id}, activando...`);
+            simpleLog(`ID es: ${id}, activando...`);
+            const data = new url.URLSearchParams({
                 btn_veri: 'yes1',
                 btn_ver: 'yes',
-            }
-            params[id] = 'Pulsa aquí para activar'
-            const data = new url.URLSearchParams(params);
-            const r = await axios({
+                [id]: 'Pulsa aquí para activar'
+            });
+            res = await axios({
                 method: 'post',
                 url: 'https://sv.spliktv.xyz/activar',
                 data: data.toString(),
@@ -33,21 +46,38 @@ const main = async() => {
                     "Referer": "https://sv.spliktv.xyz/activar",
                     "Origin": "https://sv.spliktv.xyz",
                     "Content-Length": Buffer.byteLength(data.toString())
-                },
+                }
             });
-            const body2 = r.data.toLowerCase();
-            console.log(r);
-            if (!body2.includes('error')) {
-                console.log('Activado exitosamente...');
+            html = res.data;
+            $ = cheerio.load(html);
+            const msgActivaste = $('#success').first().text().length > 0 ? $('#success').first().text() : null;
+            if (msgActivaste === 'Error!') {
+                const msgInfo = $('#info').first().text().length > 0 ? $('#info').first().text() : null;
+                console.log(`${msgActivaste} ${msgInfo}`);
+                simpleLog(`${msgActivaste} ${msgInfo}`);
+                setTimeout(main, 5000);
+            }
+            else if (msgActivaste === 'Activado!') {
+                console.log(msgActivaste);
+                simpleLog(msgActivaste);
+                setTimeout(main, 1000*60*60);
             }
             else {
-                console.log('Ocurrió un error');
+                console.log('Creo que banearon, revisa el html');
+                simpleLog('Creo que banearon, revisa el html');
+                await fs.promises.writeFile('ban.html', html, 'utf8');
+                return;
             }
-            setTimeout(main, 5000);
+        }
+        else {
+            console.log(`${msgActivaste} ${msgHora}`);
+            simpleLog(`${msgActivaste} ${msgHora}`);
+            setTimeout(main, 1000*60*60);
         }
     }
     catch (e) {
         console.error(e);
+        simpleLog(e.message);
         setTimeout(main, 5000);
     }
 
