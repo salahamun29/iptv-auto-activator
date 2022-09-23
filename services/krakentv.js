abf810c724f200a9fc94910299f4bcb0ac43b23a
xfrcc <47122480+xfrcc@users.noreply.github.com>
2022-09-23 13:37:56 -0300

    Actualizado kraken TV
    Hicieron una API dedicada a la activación que da todo lo necesario, gracias por hacerlo más facil :)

--- services/krakentv.js
+++ services/krakentv.js
@@ -1,63 +1,53 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
 const url = require('url');
 const simpleLog = require('../utils/simplelog.js');
 const serviceName = 'Kraken TV';
 const ua = 'Mozilla/5.0 (Linux; Android 10; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Mobile Safari/537.36';
 
 
-const exec = async() => {
-    try {
-        let res = await axios({
-            method: 'get',
-            url: 'https://www.listasiptvactualizadas.com/activar-kraken-tv/',
-            headers: {
-                'User-Agent': ua
-            }
-        });
-        let html = res.data;
-        let $ = cheerio.load(html);
-        const msgActivaste = $('#activarspliktv').first().text().length > 0 ? $('#activarspliktv').first().text() : null;
-        if (!msgActivaste) {
-            const token = $('input[name="token"]').val();
-            simpleLog(`Token: ${token}, activando...`);
-            const data = new url.URLSearchParams({
-                method_name: 'activate',
-                token: token
-            });
-            res = await axios({
-                method: 'post',
-                url: 'https://www.listasiptvactualizadas.com/activar-kraken-tv',
-                data: data.toString(),
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'User-Agent': ua,
-                    'Referer': 'https://www.listasiptvactualizadas.com/activar-kraken-tv',
-                    'Origin': 'https://www.listasiptvactualizadas.com'
-                }
-            });
-            html = res.data;
-            $ = cheerio.load(html);
-            const msgActivaste = $('#activarspliktv').first().text().length > 0 ? $('#activarspliktv').first().text() : null;
-            if (msgActivaste.includes('Seras bloqueado por usar diferentes metodos, ponte en contacto con el admin.')) {
-                simpleLog(serviceName, msgActivaste);
-                return;
-            }
-            else if (msgActivaste.includes('Activado..!')) {
-                simpleLog(serviceName, msgActivaste);
-                setTimeout(exec, 1000 * 60 * 60);
-            }
+const getStatus = () => new Promise(resolve => {
+    axios({
+        method: 'POST',
+        url: 'https://sv.krakentv.app/web/api.php',
+        data: 'method_name=check_out',
+        headers: {
+            'User-Agent': ua
         }
-        else {
-            simpleLog(serviceName, msgActivaste);
-            setTimeout(exec, 1000 * 60 * 60);
-        }
-    }
-    catch (e) {
-        simpleLog(serviceName, JSON.stringify(e));
-        setTimeout(exec, 10000);
-    }
+    }).then(response => resolve(response.data)).catch(error => resolve(error.response.data));
+});
 
+
+const activate = token => new Promise(resolve => {
+    const body = new url.URLSearchParams({
+        method_name: 'activate',
+        token: token
+    }).toString();
+    axios({
+        method: 'POST',
+        url: 'https://sv.krakentv.app/web/api.php',
+        data: body,
+        headers: {
+            'User-Agent': ua
+        }
+    }).then(response => resolve(response.data)).catch(error => resolve(error.response.data));
+});
+const exec = async() => {
+    let status = await getStatus();
+    if (status.code === 200 && status.message == 'You already activate') {
+        simpleLog(serviceName, 'Ya has activado');
+        setTimeout(exec, 1000 * 60 * 60);        
+    }
+    else if (status.token) {
+        status = await activate(status.token);
+        if (status.code === 200 && status.message == 'Successful activate') {
+            simpleLog(serviceName, 'Activado exitosamente');
+            setTimeout(exec, 1000 * 60 * 60);        
+        }
+    }
+    else {
+        simpleLog(serviceName, 'Servidor en mantenimiento...');
+        setTimeout(exec, 60000);        
+    }
 }
 
 module.exports = {
